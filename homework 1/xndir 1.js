11d197faef3c69c4f62838bf18f3bff7b9d96180
gor017 <85189675+gor017@users.noreply.github.com>
2021-06-10 16:37:18 +0400

    Delete homework 1 directory

--- homework 1/xndir 1.js
+++
@@ -1,8 +0,0 @@
-// xndir 1
-let num = 123
-if((num % 2 === 0) && (num !== 2)) {
-    //grel em yndhanur parz ev bard tveri bandzev(qani vor 2y nuynpes parz tiv e)
-    console.log('even')
-} else {
-    console.log('odd')
-}
--- homework 1/xndir 3.js
+++
@@ -1,7 +0,0 @@
-// xndir 3
-let a = 45
-let b = -12
-let c = 0
-let d = 3
-let e = -15
-let avg = (a + b + c + d + e) / 5
