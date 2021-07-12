251ce40da4aad9db9057f8678b1e64dfe99f24c2
gor017 <85189675+gor017@users.noreply.github.com>
2021-07-12 17:54:46 +0400

    Update Homework 4.js

--- Homework 4.js
+++ Homework 4.js
@@ -13,6 +13,25 @@ function array (arr) {
     console.log(arr1, arr2)
 }
 
+//xndir 4 (Given an array of numbers. Create an array containing only unique elements.)
+function unique (arr) {
+    let res = []
+    for (let i = 0; i < arr.length; i++) {
+        let isIncluded = false
+        for (let j = 0; j < res.length; j++) {
+            if (arr[i] === res[j]) {
+                isIncluded = true
+                break
+            }
+        }
+        if (isIncluded !== true) {
+            res.push(arr[i])
+        }
+    }
+return res 
+}
+unique([1, 4, 5, 3, 7, 8, 5, 4, 2, 4, 3])
+
 
 
 //xndir 5 (Given a string and symbols. Change first symbol by the second one in the string.)
